5a9f18668387996cfe94a8662e22aeab69bb02da
Bruno Siqueira <projetointegrador792@gmail.com>
2023-04-08 18:08:51 -0300

    Query elements finished and query multiple elements init

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -4,14 +4,61 @@ import { Application } from "./application";
 describe("Application", () => {
   test("renders correctly", () => {
     render(<Application />);
-    const nameElement = screen.getByRole("textbox");
+
+    const imageElement = screen.getByAltText(/bruno li/i);
+    expect(imageElement).toBeInTheDocument();
+
+    const customElement = screen.getByTestId("custom-element");
+    expect(customElement).toBeInTheDocument();
+
+    const closeElement = screen.getByTitle("close");
+    expect(closeElement).toBeInTheDocument();
+
+    const pageHeading = screen.getByRole("heading", {
+      //   name: "Job application form",
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole("heading", {
+      level: 2,
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
     expect(nameElement).toBeInTheDocument();
+
+    const nameElement2 = screen.getByLabelText("Name", {
+      selector: "input",
+    });
+    expect(nameElement2).toBeInTheDocument();
+
+    const nameElement3 = screen.getByPlaceholderText("Fullname");
+    expect(nameElement3).toBeInTheDocument();
+
+    const nameElement4 = screen.getByDisplayValue(/Joao siqueira/i);
+    expect(nameElement4).toBeInTheDocument();
+
+    const paragraphElement = screen.getByText(/All fields are Mandatory/i);
+    expect(paragraphElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+    expect(bioElement).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
 
     const termsElement = screen.getByRole("checkbox");
     expect(termsElement).toBeInTheDocument();
+
+    const termsElement2 = screen.getByLabelText(
+      "I agree to the terms and conditions"
+    );
+    expect(termsElement2).toBeInTheDocument();
 
     const submitButtonElement = screen.getByRole("button");
     expect(submitButtonElement).toBeInTheDocument();
--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -1,28 +1,45 @@
 export const Application = () => {
   return (
-    <form action="">
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-      </div>
-      <div>
-        <label htmlFor="job-location">Job location</label>
-        <select id="job-location">
-          <option value="">Select a country</option>
-          <option value="US">United States</option>
-          <option value="GB">United Kingdom</option>
-          <option value="CA">Canada</option>
-          <option value="IN">India</option>
-          <option value="AU">Australia</option>
-        </select>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" id="terms" />
-          conditions
-        </label>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+    <>
+      <h1>Job application form</h1>
+      <h2>Section 1</h2>
+      <p>All fields are mandatory</p>
+      <span title="close">X</span>
+      <img src="https://github.com/brunoyottabyte.png" alt="Bruno Lindu" />
+      <div data-testid="custom-element">Custom HTML element</div>
+      <form action="">
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Fullname"
+            value="Joao Siqueira"
+          />
+        </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <input type="text" id="bio" />
+        </div>
+        <div>
+          <label htmlFor="job-location">Name</label>
+          <select id="job-location">
+            <option value="">Select a country</option>
+            <option value="US">United States</option>
+            <option value="GB">United Kingdom</option>
+            <option value="CA">Canada</option>
+            <option value="IN">India</option>
+            <option value="AU">Australia</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" id="terms" />I agree to the terms and
+            conditions
+          </label>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </>
   );
 };
